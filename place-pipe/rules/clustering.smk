5db1dc6bd5b8140387e57e0c97192735d2f8bc59
Pierre Barbera <pierre.pbdas@gmail.com>
2022-07-28 16:56:14 +0200

    added snakeparser-based swarm integration

--- place-pipe/rules/clustering.smk
+++ place-pipe/rules/clustering.smk
@@ -5,27 +5,18 @@
 rule cluster_swarm:
     input:
         get_sample_fasta
-    output:
-        fasta       = "{outdir}/clustered/{sample}/sequences.fa",
-        otu_table   = "{outdir}/clustered/{sample}/otu_table.tsv"
     params:
         differences = config["params"]["swarm"]["differences"],
-        fastidious  = (" --fastidious" if config["params"]["swarm"]["fastidious"] else ""),
-        extra       = config["params"]["swarm"]["extra"]
+        fastidious  = config["params"]["swarm"]["fastidious"]
+    output:
+        seeds           = "{outdir}/clustered/{sample}/sequences.fa",
+        statistics_file = "{outdir}/clustered/{sample}/otu_table.tsv"
     threads:
         get_highest_override( "swarm", "threads" )
     log:
         "{outdir}/logs/swarm/{sample}.log"
     conda:
         "../envs/swarm.yaml"
-    script:
-        "swarm --seeds {output.fasta}"
-        " --statistics-file {output.otu_table}"
-        " --differences {params.differences}"
-        " --append-abundance 1"
-        " --log {log}"
-        "{params.fastidious}"
-        " {input}"
 
 rule cluster_dada2:
     input:
---
+++ place-pipe/scripts/swarm.py
@@ -0,0 +1,59 @@
+# =================================================================================================
+#     Dependencies and Setup
+# =================================================================================================
+
+from snakemake.shell import shell
+sys.path.insert(0, '../common')
+import snakeparser as sp
+import os
+
+shell.executable("bash")
+
+# Get the output directory
+sample_outdir = os.path.dirname( snakemake.output[0] )
+
+# =================================================================================================
+#     Parse arguments
+# =================================================================================================
+ps = sp.Parser( "swarm", snakemake )
+
+# Output args
+ps.add( snakemake.output.seeds,             "--seeds {}" )
+ps.add( snakemake.output.statistics_file,   "--statistics-file {}" )
+
+# General options
+ps.add_threads()
+
+# Clustering options
+ps.add_opt( "differences",      "--differences {}",     sp.typ.UINT )
+ps.add_opt( "no_otu_breaking",  "--no-otu-breaking",    sp.typ.FLAG )
+# Fastidious options
+ps.add_opt( "boundary",         "--boundary {}",        sp.typ.UINT )
+ps.add_opt( "ceiling",          "--ceiling {}",         sp.typ.UINT )
+ps.add_opt( "fastidious",       "--fastidious",         sp.typ.FLAG )
+ps.add_opt( "bloom_bits",       "--bloom-bits {}",      sp.typ.UINT )
+
+# (other) output options
+ps.add_opt( "append_abundance",     "--append-abundance {}",    sp.typ.UINT )
+ps.add_opt( "internal_structure",   "--internal-structure {}" )
+ps.add_opt( "network_file",         "--network-file {}" )
+ps.add_opt( "output_file",          "--output-file {}" )
+ps.add_opt( "mothur",               "--mothur",                 sp.typ.FLAG )
+ps.add_opt( "uclust_file",          "--uclust-file {}" )
+ps.add_opt( "usearch_abundance",    "--usearch-abundance",      sp.typ.FLAG )
+
+
+# Pairwise alignment advanced options
+ps.add_opt( "match_reward",         "--match-reward {}",            sp.typ.UINT )
+ps.add_opt( "mismatch_penalty",     "--mismatch-penalty {}",        sp.typ.UINT )
+ps.add_opt( "gap_opening_penalty",  "--gap-opening-penalty {}",     sp.typ.UINT )
+ps.add_opt( "gap_extension_penalty","--gap-extension-penalty {}",   sp.typ.UINT )
+
+# Required fasta input file at the end
+ps.add( snakemake.input[0], "{}",    sp.typ.FILE )
+
+# =================================================================================================
+#     Run
+# =================================================================================================
+
+shell( ps.get_shell_string() )
